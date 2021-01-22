3ba814028d818e91d1b15efc953fb61fd46613dc
Dmitry <49843752+BullitStudio@users.noreply.github.com>
2021-01-22 20:43:35 +0200

    Add upload image

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+
+const toBase64 = (file: File): Promise<string | ArrayBuffer | null> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
 
 function App() {
+  const { watch, register } = useForm();
+  const imageUpload: FileList = watch("image-upload");
+  const [previewImage, setPreviewImage] = useState<string | undefined>();
+
+  useEffect(() => {
+    if (imageUpload && imageUpload[0]) {
+      toBase64(imageUpload[0]).then(base64 => {
+        base64 && setPreviewImage(base64.toString());
+      });
+    }
+  }, [imageUpload]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {previewImage && (
+        <img
+          src={previewImage}
+          alt={"image"}
+          style={{ width: "64px", height: "64px" }}
+        />
+      )}
+      <input name={"image-upload"} type={"file"} ref={register} />
+    </>
   );
 }
 
